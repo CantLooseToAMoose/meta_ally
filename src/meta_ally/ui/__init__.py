f1111b91b0f5180cae64c2d1dd39fec01d31caf3
CantLooseToAMoose <JohannesSchillberg@web.de>
2026-01-20 12:41:49 +0100

    refactor: clean up imports and update __all__ in ui module

--- src/meta_ally/ui/__init__.py
+++ src/meta_ally/ui/__init__.py
@@ -1,27 +1,41 @@
 """User interface components and utilities."""
 
 from meta_ally.ui.conversation_saver import (
-    ConversationSaver,
+    list_saved_conversations,
+    load_conversation,
+    save_conversation,
     save_conversation_html,
-    save_conversation_json,
 )
-from meta_ally.ui.human_approval_callback import (
-    HumanApprovalCallback,
-    create_human_approval_callback,
-)
+from meta_ally.ui.human_approval_callback import create_human_approval_callback
 from meta_ally.ui.terminal_chat import start_chat_session
 from meta_ally.ui.visualization import (
-    create_variants_visualization,
-    visualize_case_variants,
+    console,
+    create_summary_table,
+    display_chat_message,
+    display_conversation_timeline,
+    display_orchestrator_message,
+    display_specialist_run,
+    show_side_by_side_comparison,
+    visualize_dataset,
+    visualize_dataset_comparison,
+    visualize_single_case,
 )
 
 __all__ = [
-    "ConversationSaver",
-    "HumanApprovalCallback",
+    "console",
     "create_human_approval_callback",
-    "create_variants_visualization",
+    "create_summary_table",
+    "display_chat_message",
+    "display_conversation_timeline",
+    "display_orchestrator_message",
+    "display_specialist_run",
+    "list_saved_conversations",
+    "load_conversation",
+    "save_conversation",
     "save_conversation_html",
-    "save_conversation_json",
+    "show_side_by_side_comparison",
     "start_chat_session",
-    "visualize_case_variants",
+    "visualize_dataset",
+    "visualize_dataset_comparison",
+    "visualize_single_case",
 ]
